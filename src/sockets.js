0d1bdb9d35c2ef1b0ae8c4d7f3cd8c61e16e61e1
Priyanshu88064 <priyanshu88064@gmail.com>
2022-12-04 21:56:13 +0530

    added prod env variable

--- src/sockets.js
+++ src/sockets.js
@@ -2,8 +2,7 @@ import io from "socket.io-client";
 
 const skt = ()=>{
     if(process.env.NODE_ENV==="development"){
-        return io("http://192.168.204.125");
-        // return io(process.env.REACT_APP_SER_LOC);
+        return io(process.env.REACT_APP_SER_LOC);
     }
     else{
         return io(process.env.REACT_APP_SER_PRO);
